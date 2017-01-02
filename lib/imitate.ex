6356e7f02030147904816e9a1c0ff7c2cd3e5988
Graeme Coupar <grambo@grambo.me.uk>
2017-01-02 14:34:08 +0000

    Add Imitate.module for generating fake modules.

--- lib/imitate.ex
+++ lib/imitate.ex
@@ -1,2 +1,45 @@
 defmodule Imitate do
+  @moduledoc """
+  Imitate provides a way of generating mock modules for injecting into tests.
+
+  These modules will have all of the functions of the original module, but
+  record their calls by passing messages back to the test process.
+  """
+
+  @doc """
+  Creates mock functions for each of the functions defined in `module_name`.
+  """
+  defmacro module(module) do
+    {:__aliases__, _, module_path} = module
+    module_info = Module.concat(module_path).module_info
+
+    exports =
+      module_info
+      |> Keyword.get(:exports)
+      |> Enum.reject(fn ({name, _}) -> name in [:__info__, :module_info] end)
+
+    for {name, arity} <- exports do
+      args = for num <- 1..arity do
+        "arg#{num}" |> String.to_atom |> Macro.var(__MODULE__)
+      end
+
+      quote do
+        def unquote(name)(unquote_splicing(args)) do
+          Agent.get(__MODULE__, fn (parent) -> parent end)
+          |> send({Imitate.Call, unquote(name), {unquote_splicing(args)}})
+        end
+      end
+    end
+  end
+
+  @doc """
+  Starts the process needed for an imitation module to work properly.
+
+  This should be called once for each imitation involved in a test, and should
+  be called from the test process.
+  """
+  def start_link(module) do
+    parent = self
+    Agent.start_link(fn -> parent end, name: module)
+  end
 end
--- test/imitate_test.exs
+++ test/imitate_test.exs
@@ -1,8 +1,39 @@
+defmodule OriginalModule do
+  def funktion_one(_an_arg) do
+    raise "This should never be called"
+  end
+
+  def funktion_two(_an_arg, _another_arg) do
+    raise "This should never be called"
+  end
+end
+
 defmodule ImitateTest do
   use ExUnit.Case
   doctest Imitate
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias Imitate
+
+  defmodule FakeModule do
+    require Imitate
+    Imitate.module(OriginalModule)
+  end
+
+  test "that FakeModule has the same functions as OriginalModule" do
+    assert OriginalModule.module_info[:exports] == FakeModule.module_info[:exports]
+  end
+
+  test "that calls in FakeModule are recorded as messages to the current process." do
+    Imitate.start_link(FakeModule)
+
+    FakeModule.funktion_one("test")
+    assert_receive {Imitate.Call, :funktion_one, {"test"}}
+    refute_receive _
+
+    FakeModule.funktion_one("test")
+    FakeModule.funktion_two("test", "oh test")
+    assert_receive {Imitate.Call, :funktion_one, {"test"}}
+    assert_receive {Imitate.Call, :funktion_two, {"test", "oh test"}}
+    refute_receive _
   end
 end
